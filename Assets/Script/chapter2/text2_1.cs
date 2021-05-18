c75a037167025b8c26780e443339e9519ac748ed
Kim Su Yeon <69496570+haeni0117@users.noreply.github.com>
2021-05-18 19:11:30 +0900

    FIX안방문열기#2-1

--- Assets/Script/chapter2/text2_1.cs
+++ Assets/Script/chapter2/text2_1.cs
@@ -17,6 +17,7 @@ public class text2_1 : MonoBehaviour
     private double waitingTime = 0.5;
     //B U T T O N
     public GameObject theonlychoice; //안방문을 연다(1개선택)
+    //public Text theonlychoice_; //<안방문을 연다> 텍스트무효화 -> text를 gameobject안에 넣어도 되나?
     
 
     
@@ -32,6 +33,7 @@ public class text2_1 : MonoBehaviour
     private static string text_4 = "한참을 고민했지만, \n중앙 통제 시스템의 위치도,\n사용 설명서의 위치도 \n생각나지 않는다.\n누군가 기억을 \n깨끗하게 지워버린 것 같다.\n10년이 넘도록 \n스마트홈 인공지능에 \n익숙해진 탓이겠지.";
     private static string text_5 = "카라를 복구하려면 \n일단은 이 우중충한 안방을 \n어서 벗어나야 한다.";
     private static string text_6 = "\"철컥, 철컥\"\n\n예상과는 달리,\n거실로 나가는 문은 잠겨 있다.";
+    private static string text_7 ="안방은 가장 보안이 \n삼엄한 공간이기 때문에 \n전자동 문이 고장 났을 경우 \n수동으로 빠져나갈 수 있도록 \n스페어 키를 서랍장에 넣어 놨다.\n번호 패드를 쓰는 방법도 있지만….\n그러려면 나사를 먼저 풀어야 한다.\n우선 서랍장을 조사해 봐야겠다.";
 
 
 
@@ -47,7 +49,8 @@ public class text2_1 : MonoBehaviour
     static IEnumerator coroutine2 = Typing(text_3);
     static IEnumerator coroutine3 = Typing(text_4);
     static IEnumerator coroutine4 = Typing(text_5);
-    static IEnumerator coroutine5 = Typing(text_6); 
+    static IEnumerator coroutine5 = Typing(text_6);
+    static IEnumerator coroutine6 = Typing(text_7);
     // static IEnumerator coroutine5 = Typing(text_6);
     //identifier : IEnumerator
 
@@ -69,14 +72,15 @@ public class text2_1 : MonoBehaviour
             yield return new WaitForSeconds(0.05f);
 
             if(num%3==1){
-                Debug.Log("coroutine is stopped");
                 num++;
+                Debug.Log("coroutine is stopped+num"+num);
                 yield break; //코루틴종료
                 //여기서 그냥 끊고, update에서 다음 시나리오 출력해주기
             }//코루틴이 중간에 끝나는 것이 아니라 그냥 나가면?
            
             }
             num++;
+            Debug.Log("user didn't skip the text+num"+num);
             //button_skip.onClick.AddListener(StopCoroutine(coroutine));
                 // button_skip = transform.Find("skipButton").GetComponent<Button>() ;
                 // button_skip.onClick.AddListener(StopCoroutine("Typing",text_1)) ;
@@ -88,14 +92,20 @@ public class text2_1 : MonoBehaviour
     } 
 
     public void theonlychoiceF(){
-        Debug.Log("you clicked <theonlybutton>");
-        num=100;
-        theonlychoice.SetActive(false);
+        //Debug.Log("you clicked <theonlybutton> num="+num);
+        Debug.Log("철컥철컥  num"+num);
+        StartCoroutine(coroutine5);
+        num=30;
+        Text theonlychoice_;
+        theonlychoice_=GameObject.Find("theonlychoice_text").GetComponent<Text>();
+        theonlychoice_.text=" "; //text-> 비우기
+            //theonlychoice.text=" ";
+        
 
     }
     
     public void coroutine_stop(){
-        Debug.Log("skipbutton is clicked! num = "+num);
+        Debug.Log("skipbutton is clicked! num="+num);
         num+=1;
         
     }
@@ -193,36 +203,35 @@ public class text2_1 : MonoBehaviour
     void Update(){
         //Debug.Log(num);
         if(num==2){
-            Debug.Log("coroutine1 is started!");
+            Debug.Log("coroutine1 is started!  num"+num);
             StartCoroutine(coroutine1); 
             num++;
             //StartCoroutine(coroutine1);
         }
         if(num==5){
-            Debug.Log("coroutine2 is started!");
+            Debug.Log("coroutine2 is started!  num"+num);
             StartCoroutine(coroutine2);
             num++;
         }
         if(num==8){
-            Debug.Log("coroutine3 is started!");
+            Debug.Log("coroutine3 is started!  num"+num);
             StartCoroutine(coroutine3);
             num++;
         }
         if(num==11){
             StartCoroutine(coroutine4);
-            Debug.Log("coroutine3 is started!");
+            Debug.Log("coroutine3 is started!  num"+num);
             num++;
         }
         if(num==14){
-            Debug.Log("<theonlychoice>버튼활성화");
+            Debug.Log("<theonlychoice>버튼활성화  num"+num);
             theonlychoice.SetActive(true);//activate button
         }
-        if(num==100){
-            Debug.Log("철컥철컥");
-            StartCoroutine(coroutine5);
-            
-        }
-        if(num==102){
+        
+        if(num==32){
+            Debug.Log("coroutine6 is started+num"+num);
+            StartCoroutine(coroutine6);
+            num++;
             
         }
        
