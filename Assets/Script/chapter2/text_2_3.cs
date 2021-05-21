e24365555b534a85e35aeee1044f51596cf63216
Kim Su Yeon <69496570+haeni0117@users.noreply.github.com>
2021-05-21 13:05:13 +0900

    REFACTOR버튼,안방조사하기#2-3

--- Assets/Script/chapter2/text_2_3.cs
+++ Assets/Script/chapter2/text_2_3.cs
@@ -13,28 +13,29 @@ public class text_2_3 : MonoBehaviour
     public static string message;
     public float m_Speed;
     private static int num=0;
+    public GameObject 안방조사;
     //B U T T O N
     public GameObject theonlychoice; //안방문을 연다(1개선택)
     //public Text theonlychoice_; //<안방문을 연다> 텍스트무효화 -> text를 gameobject안에 넣어도 되나?
 
 
 
-    private static string text_1_2="이이이이ㅣ렇듯 \n기계적으로 정돈되어 있고, \n흐트러진 이부자리만이 \n사람의 흔적을 알린다.";
-    private static string text_2 ="나는 안방에서 다시 한번 \n카라를 소리쳐 불렀지만,\n여전히 카라는 아무런 답이 없다.";
-    private static string text_3 = "스마트홈 중앙 통제 시스템이\n어디에 있더라…….\n카라를 이식한 첫날 받은 \n사용 설명서에 적혀 있을 텐데.";
-    private static string text_4 = "한참을 고민했지만, \n중앙 통제 시스템의 위치도,\n사용 설명서의 위치도 \n생각나지 않는다.\n누군가 기억을 \n깨끗하게 지워버린 것 같다.\n10년이 넘도록 \n스마트홈 인공지능에 \n익숙해진 탓이겠지.";
-    private static string text_5 = "카라를 복구하려면 \n일단은 이 우중충한 안방을 \n어서 벗어나야 한다.";
-    private static string text_6 = "\"철컥, 철컥\"\n\n예상과는 달리,\n거실로 나가는 문은 잠겨 있다.";
-    private static string text_7 ="안방은 가장 보안이 \n삼엄한 공간이기 때문에 \n전자동 문이 고장 났을 경우 \n수동으로 빠져나갈 수 있도록 \n스페어 키를 서랍장에 넣어 놨다.\n번호 패드를 쓰는 방법도 있지만….\n그러려면 나사를 먼저 풀어야 한다.\n우선 서랍장을 조사해 봐야겠다.";
+    private static string text_1="서랍장을 샅샅이 뒤졌지만,\n스페어 키가 보이지 않는다.\n혹시나 하는 마음에 \n서랍장 뒤편과 아래도 살펴봤지만, \n결과는 마찬가지다. ";
+    private static string text_2 ="하지만 이대로 주저앉아 \n있을 수는 없다.\n번호 패드로라도 \n안방을 탈출해야 한다.";
+    private static string text_3 = "안방에 드라이버로 \n쓸 만한 것이 있으려나? ";
+    // private static string text_4 = "한참을 고민했지만, \n중앙 통제 시스템의 위치도,\n사용 설명서의 위치도 \n생각나지 않는다.\n누군가 기억을 \n깨끗하게 지워버린 것 같다.\n10년이 넘도록 \n스마트홈 인공지능에 \n익숙해진 탓이겠지.";
+    // private static string text_5 = "카라를 복구하려면 \n일단은 이 우중충한 안방을 \n어서 벗어나야 한다.";
+    // private static string text_6 = "\"철컥, 철컥\"\n\n예상과는 달리,\n거실로 나가는 문은 잠겨 있다.";
+    // private static string text_7 ="안방은 가장 보안이 \n삼엄한 공간이기 때문에 \n전자동 문이 고장 났을 경우 \n수동으로 빠져나갈 수 있도록 \n스페어 키를 서랍장에 넣어 놨다.\n번호 패드를 쓰는 방법도 있지만….\n그러려면 나사를 먼저 풀어야 한다.\n우선 서랍장을 조사해 봐야겠다.";
     
     //scenario!
-    static IEnumerator coroutine_2 = Typing(text_1_2);
+    static IEnumerator coroutine = Typing(text_1);
     static IEnumerator coroutine1 = Typing(text_2);
     static IEnumerator coroutine2 = Typing(text_3);
-    static IEnumerator coroutine3 = Typing(text_4);
-    static IEnumerator coroutine4 = Typing(text_5);
-    static IEnumerator coroutine5 = Typing(text_6);
-    static IEnumerator coroutine6 = Typing(text_7);
+    // static IEnumerator coroutine3 = Typing(text_4);
+    // static IEnumerator coroutine4 = Typing(text_5);
+    // static IEnumerator coroutine5 = Typing(text_6);
+    // static IEnumerator coroutine6 = Typing(text_7);
 
     static IEnumerator Typing(string message)
     { 
@@ -66,18 +67,7 @@ public class text_2_3 : MonoBehaviour
         
     } 
 
-    public void theonlychoiceF(){
-        //Debug.Log("you clicked <theonlybutton> num="+num);
-        Debug.Log("철컥철컥  num"+num);
-        StartCoroutine(coroutine5);
-        num=30;
-        Text theonlychoice_;
-        theonlychoice_=GameObject.Find("theonlychoice_text").GetComponent<Text>();
-        theonlychoice_.text=" "; //text-> 비우기
-            //theonlychoice.text=" ";
-        
 
-    }
     
     public void coroutine_stop(){
         Debug.Log("skipbutton is clicked! num="+num);
@@ -87,7 +77,11 @@ public class text_2_3 : MonoBehaviour
     void Start()
     {
         Debug.Log("coroutin is started! #2-3 num"+num);
-        StartCoroutine(coroutine_2);
+        StartCoroutine(coroutine);
+    }
+    public void 안방조사하기(){
+        안방조사.SetActive(true);
+
     }
 
     // Update is called once per frame
@@ -103,31 +97,32 @@ public class text_2_3 : MonoBehaviour
             Debug.Log("coroutine2 is started!  num"+num);
             StartCoroutine(coroutine2);
             num++;
+            Invoke("안방조사하기",3.0f);
         }
-        if(num==8){
-            Debug.Log("coroutine3 is started!  num"+num);
-            StartCoroutine(coroutine3);
-            num++;
-        }
-        if(num==11){
-            StartCoroutine(coroutine4);
-            Debug.Log("coroutine3 is started!  num"+num);
-            num++;
-        }
-        if(num==14){
-            Debug.Log("<theonlychoice>버튼활성화  num"+num);
-            theonlychoice.SetActive(true);//activate button
-        }
+        // if(num==8){
+        //     Debug.Log("coroutine3 is started!  num"+num);
+        //     StartCoroutine(coroutine3);
+        //     num++;
+        // }
+        // if(num==11){
+        //     StartCoroutine(coroutine4);
+        //     Debug.Log("coroutine3 is started!  num"+num);
+        //     num++;
+        // }
+        // if(num==14){
+        //     Debug.Log("<theonlychoice>버튼활성화  num"+num);
+        //     theonlychoice.SetActive(true);//activate button
+        // }
         
-        if(num==32){
-            Debug.Log("coroutine6 is started+num"+num);
-            StartCoroutine(coroutine6);
-            fadeout.SetActive(true);
+        // if(num==32){
+        //     Debug.Log("coroutine6 is started+num"+num);
+        //     StartCoroutine(coroutine6);
+        //     fadeout.SetActive(true);
             
             
 
             
-        }
+        // }
        
 
     }
